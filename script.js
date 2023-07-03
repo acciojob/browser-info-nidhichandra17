4f019d4bae4965681797503a7583937d2ad93db9
Nidhi Chandra <117846833+nidhichandra17@users.noreply.github.com>
2023-07-03 23:19:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,4 @@
 //your JS code here. If required.
+//your JS code here. If required.
+let elem = document.getElementById("browser-info");
+elem.innerHTML = "You are using " + navigator.appName +" version " + navigator.appVersion;
